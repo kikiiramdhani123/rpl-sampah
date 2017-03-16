2bee789c26f2e18c3bdf76020b567c7dd3aed621
DHANI SULISTIYO WIBOWO <dhani.s.wibowo@gmail.com>
2017-03-16 11:44:33 +0700

    Update Structure Aplikasi
    update struktur folder untuk aplikasi, mulai dari database, dukumentasi,
    dan apps

---
+++ APTSI/application/views/Layout/Footer.php
@@ -0,0 +1,36 @@
+		<div class="footer">
+            <div class="pull-right">
+                10GB of <strong>250GB</strong> Free.
+            </div>
+            <div>
+                <strong>Copyright</strong> Example Company &copy; 2014-2015
+            </div>
+        </div>
+
+        </div>
+        </div>
+
+	<script type="text/javascript" src=" <?php echo base_url();?>asset/js/jquery-2.1.1.js"></script>
+	<script type="text/javascript" src=" <?php echo base_url();?>asset/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src=" <?php echo base_url();?>asset/lib/metisMenu/jquery.metisMenu.js"></script>
+	<script type="text/javascript" src=" <?php echo base_url();?>asset/lib/slimscroll/jquery.slimscroll.min.js"></script>
+	    <!-- Custom and plugin javascript -->
+		<script type="text/javascript" src=" <?php echo base_url();?>asset/js/inspinia.js"></script>
+		<script type="text/javascript" src=" <?php echo base_url();?>asset/lib/pace/pace.min.js"></script>	
+	<script type="text/javascript" src=" <?php echo base_url();?>asset/lib/toastr/toastr.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            setTimeout(function() {
+                toastr.options = {
+                    closeButton: true,
+                    progressBar: true,
+                    showMethod: 'slideDown',
+                    timeOut: 4000
+                };
+                toastr.success('Responsive Admin Theme', 'Welcome to INSPINIA');
+
+            }, 1300);
+        });
+    </script>
+</body>
+</html>
